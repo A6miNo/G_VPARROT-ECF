cc807617155f5595b2c0385cecb99f009eadb0b5
Amandine.Cimmino <130897448+A6miNo@users.noreply.github.com>
2023-09-06 09:15:34 +0200

    Structure site

---
+++ front/js/home.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var video = document.querySelector("#video-background video");
+
+    // Définir le coefficient de vitesse de lecture de la vidéo
+    video.playbackRate = 0.6; 
+});
