414f67fd595212b35aad825ff048a80ca72de25c
Amandine.Cimmino <130897448+A6miNo@users.noreply.github.com>
2023-09-13 17:26:59 +0200

    remove directory

--- front/js/home.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var video = document.querySelector("#video-background video");
-
-    // Définir le coefficient de vitesse de lecture de la vidéo
-    video.playbackRate = 0.6; 
-});
